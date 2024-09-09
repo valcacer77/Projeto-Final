a855ce8d9ed973379c93debb1a6520526d0394a4
Gabriel Valcacer Torquato <gabrielvalcacertorquato@gmail.com>
2024-09-09 14:44:14 -0300

    Create parte 1.sql

---
+++ parte 1.sql
@@ -0,0 +1,25 @@
+/*
+SENAC DF - CEP Jessé Freire
+2024.07.276 - Aprendizagem Profissional de Qualificação em Desenvolvimento de Softwares
+Prof: Hudson Neves
+Projeto Final - Implementação de Banco de Dados Relacional com MySQL
+ Projeto - Clínica Odontológica Odontovida */
+INSERT INTO Paciente (nome, data_nascimento, telefone, endereco, email) VALUES
+('Ana Silva', '1985-04-15', '11987654321', 'Rua das Flores, 123, São Paulo', 'ana.silva@email.com'),
+('Carlos Oliveira', '1990-06-30', '21987654321', 'Avenida Paulista, 456, São Paulo', 'carlos.oliveira@email.com'),
+('Fernanda Costa', '1972-12-03', '31987654321', 'Rua das Acácias, 789, Belo Horizonte', 'fernanda.costa@email.com'),
+('Maria Souza', '1995-09-10', '51987654321', 'Rua do Mercado, 302, Rio de Janeiro', 'maria.souza@email.com'),
+('Pedro Lima', '1988-07-25', '81987654321', 'Praça da Liberdade, 455, Rio de Janeiro', 'pedro.lima@email.com'),
+('Paula Ferreira', '1992-11-14', '41987654321', 'Rua dos Pinheiros, 403, Porto Alegre', 'paula.ferreira@email.com'),
+('Roberto Almeida', '1981-05-15', '71987654321', 'Rua dos Cravos, 230, Salvador', 'roberto.almeida@email.com'),
+('Sofia Martins', '1998-12-08', '61987654321', 'Rua das Orquídeas, 69, Curitiba', 'sofia.martins@email.com'),
+('Tiago Pereira', '1981-08-25', '31987654321', 'Rua dos Lírios, 707, Curitiba', 'tiago.pereira@email.com');
+INSERT INTO dentista (nome, especialidade, telefone, email) VALUES
+('Dr. João Mendes', 'Ortodontia', '11912345678', 'joao.mendes@email.com'),
+('Dra. Maria Oliveira', 'Endodontia', '21912345678', 'maria.oliveira@email.com'),
+('Dr. Pedro Silva', 'Periodontia', '81912345678', 'pedro.silva@email.com'),
+('Dr. Fernando Santos', 'Odontopediatria', '41912345678', 'fernando.santos@email.com'),
+('Dr. Paulo Lima', 'Implantes', '51912345678', 'paulo.lima@email.com'),
+('Dra. Juliana Costa', 'Estética', '61912345678', 'juliana.costa@email.com'),
+('Dr. Ricardo Almeida', 'Cirurgia Geral', '71912345678', 'ricardo.almeida@email.com'),
+('Dra. Laura Ferreira', 'Radiologia', '11923456789', 'laura.ferreira@email.com');
